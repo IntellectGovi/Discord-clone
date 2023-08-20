68633ebcbef984384666fa53b57661a366ebd6ba
G O V I N D   U P A D H Y A Y <govind.upadhayay19@gmail.com>
2023-08-20 23:24:23 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  fontfamily:{
+    Whitney:["Whitney"],
+    Ginto:["Ginto"],
+    ggSans:["ggSans"],
+  },
+};
